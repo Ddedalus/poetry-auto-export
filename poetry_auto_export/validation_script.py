6e96c200a721aa4249ac4135d38fbbf80a60e55b
Hubert Bereś <hubert@springbok.ai>
2024-09-26 07:22:15 +0100

    Test coverage for validation script

--- poetry_auto_export/validation_script.py
+++ poetry_auto_export/validation_script.py
@@ -14,11 +14,27 @@ import hashlib
 import sys
 from pathlib import Path
 
+if len(sys.argv) > 1 and sys.argv[1] in ("-h", "--help"):
+    print(__doc__)
+    sys.exit(0)
+
+if len(sys.argv) > 3:
+    print(__doc__)
+    raise ValueError("Too many arguments")
+
 lock_file_path = sys.argv[1] if len(sys.argv) > 1 else "poetry.lock"
 requirements_file_path = sys.argv[2] if len(sys.argv) > 2 else "requirements.txt"
 
 lock_file = Path(lock_file_path)
 requirements_file = Path(requirements_file_path)
+
+if not lock_file.is_file():
+    raise FileNotFoundError(f"File not found: {lock_file}")
+elif not lock_file.suffix == ".lock":
+    raise ValueError(f"Invalid file type: {lock_file} (expected .lock file)")
+
+if not requirements_file.is_file():
+    raise FileNotFoundError(f"File not found: {requirements_file}")
 
 lock_hash = hashlib.sha1(lock_file.read_bytes()).hexdigest()
 first_line = requirements_file.read_text().split("\n")[0]
--- tests/test_validation_script.py
+++ tests/test_validation_script.py
@@ -1,6 +1,8 @@
 import subprocess
 from pathlib import Path
 
+import pytest
+
 from poetry_auto_export.plugin import PoetryAutoExport
 
 repo_root = Path(__file__).parent.parent
@@ -9,19 +11,51 @@ validation_script_path = (
 ).absolute()
 
 
-def test_validation_script_pass(
+@pytest.fixture
+def valid_project(
     basic_project: Path,
     event,
     dispatcher,
     plugin: PoetryAutoExport,
-):
-    """Execute validation_script.py using subprocess and check exit code is zero."""
-    # Given
+) -> Path:
+    """A poetry project with up-to-date generated requirements.txt file."""
     # TODO: call the plugin directly instead to save time
     subprocess.run(["poetry", "lock"], cwd=basic_project)
+    return basic_project
 
-    # When
-    exit_code = subprocess.call(["python", validation_script_path], cwd=basic_project)
 
-    # Then
+def test_validation_script_pass(valid_project: Path):
+    """Execute validation_script.py using subprocess and check exit code is zero."""
+    exit_code = subprocess.call(["python", validation_script_path], cwd=valid_project)
     assert exit_code == 0
+
+
+def test_validation_script_help(basic_project: Path):
+    """Execute validation_script.py with --help and check exit code is zero.
+    The script should also display the help message."""
+    result = subprocess.run(
+        ["python", validation_script_path, "--help"],
+        capture_output=True,
+        cwd=basic_project,
+    )
+    assert result.returncode == 0
+    assert "Usage" in result.stdout.decode()
+
+
+@pytest.mark.parametrize(
+    "file_name",
+    ["poetry.lock", "requirements.txt"],
+)
+def test_validation_script_missing_files(valid_project: Path, file_name: str):
+    """
+    Execute validation_script.py with a missing poetry.lock or requirements file.
+    Check there is non-zero exit code and suitable message.
+    """
+    (valid_project / file_name).unlink()
+
+    result = subprocess.run(
+        ["python", validation_script_path], cwd=valid_project, capture_output=True
+    )
+
+    assert result.returncode == 1
+    assert "File not found" in result.stderr.decode()
